f9ddadc8c922cd340c03be37234d61971a85c17f
08021976-22022006 <poojanas.dev@gmail.com>
2025-09-08 18:40:55 +0530

    Login frontent Simple One completed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors")
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth"); 
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 // Routes
 app.use("/auth", authRoutes);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+export default function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
